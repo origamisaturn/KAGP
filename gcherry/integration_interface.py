690089f89ed833274b24ff25456296a69fc2969b
Christopher Vargas <origamisaturn@gmail.com>
2024-05-11 23:26:32 -0400

    added more comments to transforms

--- gcherry/integration_interface.py
+++ gcherry/integration_interface.py
@@ -178,14 +178,30 @@ def unit_vector(vec):
     """
 
 def perifocal2global_rot(lan, inc, argp):
-    """ Rotation from perifocal to global axes. """
+    """ Rotation from perifocal to global axes. 
+    Args:
+        lan: [rad.] Longitude of Ascending Node
+        inc: [rad.] Inclination
+        argp: [rad.] Argument of Periapsis
+        
+    """
     return Rz(lan)@Rx(inc)@Rz(argp)
 
 def global2perifocal_rot(lan, inc, argp):
+    """ Rotation from global to perifocal axes.
+    See perifocal2global_rot().
+    
+    """
     return perifocal2global_rot(lan, inc, argp).T
 
 def pcf2global_rot(pos_global, lan, inc):
-    """ Rotation from plane control frame to global axes. """
+    """ Rotation from plane control frame to global axes. 
+    Args:
+        pos_global: [m] 3-element vector, position in global frame.
+        lan: [rad.] Longitude of Ascending Node
+        inc: [rad.] Inclination
+        
+    """
     argp = 0 # argp does not affect orbit normal vector.
     orbit_normal_global = perifocal2global_rot(lan, inc, argp)@np.array([0, 0, 1])
     x = unit_vector(pos_global)
@@ -194,25 +210,47 @@ def pcf2global_rot(pos_global, lan, inc):
     return np.stack((x, y, z), axis=-1)
 
 def global2pcf_rot(pos_global, lan, inc):
+    """ Rotation from global axes to plane control axes. 
+    See pcf2global_rot().
+    
+    """
     return pcf2global_rot(pos_global, lan, inc).T
 
 def get_ra_decl(pos_global):
-    """ Gets right ascension and declination of given position. """
+    """ Gets right ascension and declination of given position. 
+    Args:
+        pos_global: [m] 3-element vector, position in global frame.
+
+    """
     x, y, z = tuple(pos_global)
     ra = np.arctan2(y, x)
     decl = np.arctan2(z, np.linalg.norm([x, y]))
     return ra, decl
 
 def body2topo_rot(roll, pitch, yaw):
-    """ Rotation from body to topocentric axes. """
+    """ Rotation from body to topocentric axes. 
+    Args:
+        roll: [rad.]
+        pitch: [rad.]
+        yaw: [rad.]
+
+    """
     return Rz(yaw)@Ry(pitch)@Rx(roll)
 
 def topo2body_rot(roll, pitch, yaw):
-    """ Rotation from topocentric to body axes. """
+    """ Rotation from topocentric to body axes. 
+    See body2topo_rot().
+    
+    """
     return body2topo_rot(roll, pitch, yaw).T
 
 def topo2global_rot(ra, decl):
-    """ Rotation from topocentric to global axes. """
+    """ Rotation from topocentric to global axes. 
+    Args:
+        ra: [rad.] Right Ascension
+        decl: [rad.] Declination
+        
+    """
     axes_switch_rot = np.array([[0, 0, -1],
                                 [0, 1, 0],
                                 [1, 0, 0]])
@@ -220,14 +258,29 @@ def topo2global_rot(ra, decl):
     return Rz(ra)@Ry(-decl)@axes_switch_rot
 
 def global2topo_rot(ra, decl):
-    """ Rotation from global to topocentric axes. """
+    """ Rotation from global to topocentric axes. 
+    See topo2global_rot().
+    
+    """
     return topo2global_rot(ra, decl).T
 
 def body2global_rot(roll, pitch, yaw, pos_global):
+    """ Rotation from body to global axes. 
+    Args:
+        roll: [rad.]
+        pitch: [rad.]
+        yaw: [rad.]
+        pos_global: [m] 3-element vector, position in global frame.
+        
+    """
     ra, decl = get_ra_decl(pos_global)
     return topo2global_rot(ra, decl) @ body2topo_rot(roll, pitch, yaw)
 
 def global2body_rot(roll, pitch, yaw, pos_global):
+    """ Rotation from global to body axes.
+    See global2body_rot().
+        
+    """
     return body2global_rot(roll, pitch, yaw, pos_global).T
 
 
